3d4e6b23c6d81f8141577c2b871e83a039434ca4
quantumshiro <hiro.1nakanishi@gmail.com>
2024-10-03 11:46:12 +0900

    add: falcon1024 benchmark

--- benches/polytorus-bench/main.rs
+++ benches/polytorus-bench/main.rs
@@ -1,4 +1,4 @@
-use polytorus::cryptography::falcon::falcon512;
+use polytorus::cryptography::falcon::{falcon512, falcon1024};
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand::Rng;
 
@@ -12,5 +12,15 @@ fn bench_falcon512(c: &mut Criterion) {
     }));
 }
 
-criterion_group!(benches, bench_falcon512);
+fn bench_falcon1024(c: &mut Criterion) {
+    let mut rng = rand::thread_rng();
+    let msg: [u8; 5] = rng.gen();
+    let (sk, pk) = falcon1024::keygen(rng.gen());
+    c.bench_function("falcon1024", |b| b.iter(|| {
+        let sig = falcon1024::sign(&msg, &sk);
+        falcon1024::verify(&msg, &sig, &pk);
+    }));
+}
+
+criterion_group!(benches, bench_falcon512, bench_falcon1024);
 criterion_main!(benches);
